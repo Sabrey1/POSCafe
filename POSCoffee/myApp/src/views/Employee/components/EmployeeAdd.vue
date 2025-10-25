fd32066a5eeb54dde63bbec698fbb9de721d5f41
Sabrey <limsabriey94@gmail.com>
2025-10-25 15:12:56 +0700

    Updates

--- POSCoffee/myApp/src/views/Employee/components/EmployeeAdd.vue
+++ POSCoffee/myApp/src/views/Employee/components/EmployeeAdd.vue
@@ -36,17 +36,82 @@
     </ion-row>
     <ion-row>
         <ion-col>
-            <!-- Email -->
+            <!-- Adress -->
             <ion-item lines="none">
                 <ion-input :label="t('Adress')" label-placement="floating" fill="outline" :placeholder="t('Enter Adress')"></ion-input>
             </ion-item>
         </ion-col>
         <ion-col>
-             
+            <ion-item lines="none">
+                <!-- Employee Role -->
+                <ion-select aria-label="role" fill="outline" interface="popover" :placeholder="t('Select Role')">
+                    <ion-select-option value="user">{{t("User")}}</ion-select-option>
+                    <ion-select-option value="admin">{{t("Admin")}}</ion-select-option>
+                    <ion-select-option value="Manager">{{t("Manager")}}</ion-select-option>
+                </ion-select>
+            </ion-item>
         </ion-col>
     </ion-row>
-      
-      <br>
+    <ion-row>
+        <ion-col>
+            <!-- Hire Date -->
+            <ion-item lines="none">
+                <ion-input :label="t('Hire Date')" label-placement="floating" type="date" fill="outline" :placeholder="t('Enter Hire Date')"></ion-input>
+            </ion-item>
+        </ion-col>
+        <ion-col>
+            <ion-item lines="none">
+                <!-- Employee Role -->
+                <ion-select aria-label="employee_type" fill="outline" interface="popover" :placeholder="t('Select Employee Type')">
+                    <ion-select-option value="vip">{{t("VIP")}}</ion-select-option>
+                    <ion-select-option value="normal">{{t("Normal")}}</ion-select-option>
+                </ion-select>
+            </ion-item>
+        </ion-col>
+    </ion-row>
+
+    <ion-row>
+        <ion-col>
+            <ion-item lines="none">
+                <!-- Employee Position -->
+                <ion-select aria-label="position" fill="outline" interface="popover" :placeholder="t('Select Role')">
+                    <ion-select-option value="ITSupport">{{t("IT Support")}}</ion-select-option>
+                    <ion-select-option value="NetworkManager">{{t("Network Manager")}}</ion-select-option>
+                    <ion-select-option value="Manager">{{t("Manager")}}</ion-select-option>
+                </ion-select>
+            </ion-item>
+        </ion-col>
+        <ion-col>
+            <ion-item lines="none">
+                <ion-input :label="t('Salary')" label-placement="floating" type="number" fill="outline" :placeholder="t('Enter Salary')"></ion-input>
+            </ion-item>
+        </ion-col>
+    </ion-row>
+
+    <ion-row>
+       <ion-col>
+             <ion-item lines="none">
+                <!-- Employee Position -->
+                <ion-select aria-label="status" fill="outline" interface="popover" :placeholder="t('Select Status')">
+                    <ion-select-option value="active">{{t("Active")}}</ion-select-option>
+                    <ion-select-option value="inactive">{{t("Inactive")}}</ion-select-option>
+                </ion-select>
+            </ion-item>
+        </ion-col>
+        <ion-col>
+            <ion-item lines="none">
+                <ion-input :label="t('Note')" label-placement="floating" fill="outline" :placeholder="t('Enter Note')"></ion-input>
+            </ion-item>
+        </ion-col>
+    </ion-row>
+    <ion-row>
+       <ion-col>
+               <ion-checkbox label-placement="end">Disabled</ion-checkbox>
+        </ion-col>
+        <ion-col>
+            
+        </ion-col>
+    </ion-row>    
       <ion-button expand="block" color="primary" @click="close">{{t("Save")}}</ion-button>
       </ion-grid>
     </div>
@@ -54,7 +119,7 @@
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonItem, IonLabel, IonInput, IonButton, modalController, IonList, IonSelect, IonSelectOption  } from '@ionic/vue';
+import { IonContent, IonItem, IonLabel, IonInput, IonButton, modalController, IonList, IonSelect, IonSelectOption,IonCheckbox  } from '@ionic/vue';
 
 const t = window.t
 
@@ -62,7 +127,5 @@ const close = async () => {
   const modal = await modalController.getTop();
   if (modal) await modal.dismiss();
 };
-
-
 
 </script>
