3372a4993bf1b21588e7c3612bf5a01d568bcf78
Sabrey <limsabriey94@gmail.com>
2025-10-31 16:03:27 +0700

    Update

---
+++ POSCoffee/myApp/src/views/WorkingDay/components/FooterWorkingDay.vue
@@ -0,0 +1,16 @@
+<template>
+    <ion-footer>
+        <ion-toolbar>
+            <ion-title class="ion-text-center">
+                 <ion-button color="danger">{{ t("Cancel") }}</ion-button>
+                 <ion-button color="warning">{{ t("View Working Day") }}</ion-button>
+                 <ion-button color="success">{{ t("Open Working Day") }}</ion-button>
+            </ion-title>
+        </ion-toolbar>
+    </ion-footer>
+</template>
+
+<script setup lang="ts">
+import { IonFooter, IonTitle, IonToolbar } from '@ionic/vue';
+const t = window.t
+</script>
--- POSCoffee/myApp/src/views/WorkingDay/workingDay.vue
+++ POSCoffee/myApp/src/views/WorkingDay/workingDay.vue
@@ -2,12 +2,69 @@
 <ion-page>
         <ToolBar>{{(t("Working Day"))}}</ToolBar> 
     <ion-content>
-        This is Working Day Page
+        <div class="p-2 fix-container">
+            <ion-grid>
+                <ion-row>
+                    <ion-col>
+                        <ion-input
+                            :label="t('Working Day')"
+                            label-placement="floating"
+                            type="date"
+                            fill="outline"
+                            :placeholder="t('Select Working Day')"
+                        />
+                    </ion-col>
+                    <ion-col>
+                        <ion-input
+                            :label="t('POS Profile')"
+                            label-placement="floating"
+                            fill="outline"
+                            :placeholder="t('Enter POS Profile')"
+                        />
+                    </ion-col>
+                </ion-row>
+                <ion-row>
+                    <ion-col>
+                       <ion-input
+                :label="t('Note')"
+                label-placement="floating"
+                type="text"
+                fill="outline"
+                :placeholder="t('Enter Note')"
+                style="height: 100px;"
+                ></ion-input>
+                    </ion-col>
+                </ion-row>
+            </ion-grid>
+        </div>
+        <FooterWorkingDay class="footer" />
     </ion-content>
 </ion-page>
 </template>
 
 <script setup lang="ts">
 import ToolBar from "@/views/Layout/ToolBar.vue"
+import FooterWorkingDay from "@/views/WorkingDay/components/FooterWorkingDay.vue"
 const t = window.t
 </script>
+
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  background-color: var(--ion-background-color, #ffffff);
+  box-shadow: 0 -2px 8px rgba(0, 0, 0, 0.1);
+}
+
+.ion-content {
+  padding-bottom: 80px;
+}
+
+.custom-note-input {
+  --padding-start: 10px;
+  height: 100px;
+}
+</style>
